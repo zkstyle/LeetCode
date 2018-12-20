c2c3c9a0088cec9f85a5054eab4f969b967d352a
elvis <712338173@qq.com>
2018-12-20 09:49:39 +0800

    leetcode

--- src/com/elvis/leetcode/hashtable/leetcode03.java
+++ src/com/elvis/leetcode/hashtable/leetcode03.java
@@ -53,6 +53,26 @@ public class leetcode03 {
 
     }
 
+    /**
+     * 加强版
+     * @param s
+     * @return
+     */
+    public static int lengthOfLongestSubstringPlus(String s) {
+        if (s.length() == 0) return 0;
+        int maxLength = 0;
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        for (int i = 0 ,j = 0 ; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))){
+                j = Math.max(j, map.get(s.charAt(i))+1);
+            }
+            map.put(s.charAt(i), i);
+            maxLength = Math.max(maxLength, i - j + 1);
+        }
+        return maxLength;
+    }
+
+
     public static void main(String[] args) {
         int length = lengthOfLongestSubstring("jbpnbwwd");
         System.out.println(length);
--- src/com/elvis/leetcode/hashtable/leetcode18.java
+++ src/com/elvis/leetcode/hashtable/leetcode18.java
@@ -1,8 +1,6 @@
 package com.elvis.leetcode.hashtable;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @BelongsProject: LeetCode
@@ -29,7 +27,7 @@ import java.util.Map;
  * ]
  */
 public class leetcode18 {
-    public static List<List<Integer>> fourSum(int[] nums, int target) {
+    public static List<List<Integer>> fourSum1(int[] nums, int target) {
         List<List<Integer>> lists = null;
         List<Integer> list = null;
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
@@ -42,13 +40,59 @@ public class leetcode18 {
         return lists;
     }
 
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+        Set<List<Integer>> listSet = new HashSet<>();
+        int n = nums.length;
+        Arrays.sort(nums);//数据排序
+        HashMap<Integer, List<Integer[]>> hashMap = new HashMap<>();
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                int num = nums[i] + nums[j];
+                Integer[] pair = {i, j};
+                if(hashMap.containsKey(num)) {
+                    hashMap.get(num).add(pair);
+                }else {
+                    List<Integer[]> list = new ArrayList<>();
+                    list.add(pair);
+                    hashMap.put(num, list);
+                }
+            }
+        }
+        for(Integer integer : hashMap.keySet()) {
+            if(hashMap.containsKey(target - integer)) {
+                List<Integer[]> list1 = hashMap.get(integer);
+                List<Integer[]> list2 = hashMap.get(target - integer);
+                for(Integer[] pair1 : list1) {
+                    int index1 = pair1[0];
+                    int index2 = pair1[1];
+                    for(Integer[] pair2 : list2) {
+                        int index3 = pair2[0];
+                        int index4 = pair2[1];
+                        if(index2 < index3) {
+                            List<Integer> list = new ArrayList<>();
+                            list.add(nums[index1]);
+                            list.add(nums[index2]);
+                            list.add(nums[index3]);
+                            list.add(nums[index4]);
+                            listSet.add(list);
+                        }
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(listSet);
+    }
+
+
     public static void main(String[] args) {
         int[] nums = {1,0,-1,0,-2,2};
         int target = 0;
         List<List<Integer>> lists = fourSum(nums, target);
         for (List<Integer> list : lists) {
-            System.out.println(list.toArray().toString());
-
+            for (Object i : list.toArray()){
+                System.out.print(i+" ");
+            }
+            System.out.println();
         }
 
     }
