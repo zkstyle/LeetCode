b8a7fd05aa72319ee2feb2970fe06bdee1575e02
zkstyle <712338173@qq.com>
2019-01-22 11:34:05 +0800

    leetcode

---
+++ src/com/elvis/leetcode/linkedlist/listnode/ListNode.java
@@ -0,0 +1,18 @@
+package com.elvis.leetcode.linkedlist.listnode;
+
+/**
+ * @BelongsProject: LeetCode
+ * @BelongsPackage: com.elvis.leetcode.linkedlist.listnode
+ * @Author: Elvis
+ * @CreateTime: 2019-01-22 10:01
+ * Description: 链表
+ */
+public class ListNode {
+    //Definition for singly-linked list.
+    public int val;
+    public ListNode next;
+    public ListNode(int x) {
+        val = x;
+    }
+
+}
