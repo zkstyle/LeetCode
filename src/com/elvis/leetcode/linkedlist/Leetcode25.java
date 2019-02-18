18afc77b3704123273f45d76af725716ba0c902d
zkstyle <712338173@qq.com>
2019-02-18 21:20:43 +0800

    leetcode

---
+++ src/com/elvis/leetcode/linkedlist/Leetcode25.java
@@ -0,0 +1,162 @@
+package com.elvis.leetcode.linkedlist;
+
+import com.elvis.leetcode.linkedlist.listnode.ListNode;
+
+/**
+ * @BelongsProject: LeetCode
+ * @BelongsPackage: com.elvis.leetcode.linkedlist
+ * @Author: Elvis
+ * @CreateTime: 2019-02-16 14:57
+ * Description: k个一组翻转链表
+ * 给出一个链表，每 k 个节点一组进行翻转，并返回翻转后的链表。
+ *
+ * k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是 k 的整数倍，那么将最后剩余节点保持原有顺序。
+ *
+ * 示例 :
+ *
+ * 给定这个链表：1->2->3->4->5
+ *
+ * 当 k = 2 时，应当返回: 2->1->4->3->5
+ *
+ * 当 k = 3 时，应当返回: 3->2->1->4->5
+ *
+ * 说明 :
+ *
+ * 你的算法只能使用常数的额外空间。
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ */
+public class Leetcode25 {
+
+    /**
+     * 4ms典范
+     * @param head
+     * @param k
+     * @return
+     */
+    public ListNode reverseKGroupPlus(ListNode head, int k) {
+        ListNode prev = null;
+        ListNode cur = head;
+        ListNode next = null;
+        ListNode check =head;
+        int canProceed = 0;
+        int count = 0;
+
+        while(canProceed<k && check != null){
+            check=check.next;
+            canProceed++;
+        }
+
+        if(canProceed==k){
+            while(count<k && cur != null){
+                next = cur.next;
+                cur.next=prev;
+                prev=cur;
+                cur=next;
+                count++;
+            }
+            if(next!=null){
+                head.next=reverseKGroup(next,k);
+            }
+            return prev;
+        } else {
+            return head;
+        }
+    }
+
+    /**
+     *
+     * @param head
+     * @param k
+     * @return
+     */
+    public  ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null){
+            return head;
+        }
+        if (k == 1){
+            return head;
+        }
+        ListNode curr = head;
+        ListNode hand = head;
+        ListNode temp = null;
+        ListNode pre = null;
+
+        while (true){
+            ListNode tempHand = curr;
+            ListNode tempLast = curr;
+            for (int i = 1; i < k; i++) {
+                if (tempLast != null && tempLast.next != null){
+                    tempLast = tempLast.next;
+                    if (i == k - 1){
+                        temp = tempLast.next;
+                        tempLast.next = null;
+                    }
+                }else {
+                    if (temp != null){
+                        getLastNode(hand).next = tempHand;
+                    }
+                    return hand;
+                }
+            }
+            ListNode newNode = reverseList(tempHand);
+            if (pre != null){
+                pre.next = newNode;
+            }else {
+                hand = newNode;
+            }
+
+            while (newNode.next != null){
+                newNode = newNode.next;
+                if (newNode == null){
+                    newNode.next = temp;
+                }else {
+                    pre = newNode;
+                }
+            }
+            curr = temp;
+        }
+
+    }
+
+    public ListNode getLastNode(ListNode hand){
+        if (hand == null){
+            return hand;
+        }
+        ListNode temp = new ListNode(0);
+        temp.next = hand;
+        ListNode last = hand;
+        while (temp.next != null){
+            last = temp.next;
+            temp = temp.next;
+        }
+        return last;
+    }
+    public ListNode reverseList(ListNode head){
+        if (head == null){
+            return null;
+        }
+        ListNode next = null;
+        ListNode pre = null;
+        ListNode curr = head;
+
+        while (curr != null){
+            next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+
+        head = pre;
+        return head;
+    }
+
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(3);
+        l1.next.next.next = new ListNode(4);
+        l1.next.next.next.next = new ListNode(5);
+        new Leetcode25().reverseKGroup(l1, 4);
+    }
+}
---
+++ src/com/elvis/leetcode/linkedlist/Leetcode61.java
@@ -0,0 +1,98 @@
+package com.elvis.leetcode.linkedlist;
+
+import com.elvis.leetcode.linkedlist.listnode.ListNode;
+
+/**
+ * @BelongsProject: LeetCode
+ * @BelongsPackage: com.elvis.leetcode.linkedlist
+ * @Author: Elvis
+ * @CreateTime: 2019-02-18 15:54
+ * Description: 旋转链表
+ * 给定一个链表，旋转链表，将链表每个节点向右移动 k 个位置，其中 k 是非负数。
+ *
+ * 示例 1:
+ *
+ * 输入: 1->2->3->4->5->NULL, k = 2
+ * 输出: 4->5->1->2->3->NULL
+ * 解释:
+ * 向右旋转 1 步: 5->1->2->3->4->NULL
+ * 向右旋转 2 步: 4->5->1->2->3->NULL
+ * 示例 2:
+ *
+ * 输入: 0->1->2->NULL, k = 4
+ * 输出: 2->0->1->NULL
+ * 解释:
+ * 向右旋转 1 步: 2->0->1->NULL
+ * 向右旋转 2 步: 1->2->0->NULL
+ * 向右旋转 3 步: 0->1->2->NULL
+ * 向右旋转 4 步: 2->0->1->NULL
+ */
+public class Leetcode61 {
+    public static ListNode rotateRight(ListNode head, int k) {
+        if (head == null) return null;
+        int len = 0;
+        ListNode headPoint = head;
+        while (headPoint != null && headPoint.next != null){
+            headPoint = headPoint.next;
+            len++;
+        }
+        len++;
+        int n = k % len;
+        if (n == 0) return head;
+        ListNode flag = head;
+        for (int i = 0; i < (len - n - 1); i++) {
+            flag = flag.next;
+        }
+        ListNode tmp;
+        tmp = flag.next;
+        flag.next = null;
+        headPoint.next = head;
+        head = tmp;
+        return head;
+    }
+
+    /**
+     * 9ms典范
+     * @param head
+     * @param k
+     * @return
+     */
+    public static ListNode rotateRightPlus(ListNode head, int k) {
+        if (head == null) return null;
+        int len = 0;
+        ListNode headPoint = head;
+        while (headPoint != null){
+            headPoint = headPoint.next;
+            len++;
+        }
+        int n = k % len;
+        if (n == 0) return head;
+        int count = 0;
+        headPoint = head;
+        ListNode next = null;
+        while (headPoint != null){
+            if (count == (len - n -1)){
+                next = headPoint.next;
+                headPoint.next = null;
+                break;
+            }
+            count++;
+            headPoint = headPoint.next;
+        }
+        ListNode res = next;
+        while (next.next != null){
+            next = next.next;
+        }
+        next.next = head;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(3);
+        l1.next.next.next = new ListNode(4);
+        l1.next.next.next.next = new ListNode(5);
+        rotateRight(l1, 2);
+    }
+}
