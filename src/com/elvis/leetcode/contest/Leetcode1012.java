11c87a2fd36be65a6505b14b08f311ab7165e159
zkstyle <712338173@qq.com>
2019-03-18 13:55:33 +0800

    leetcode

---
+++ src/com/elvis/leetcode/contest/Leetcode1012.java
@@ -0,0 +1,20 @@
+package com.elvis.leetcode.contest;
+
+import static java.lang.Math.pow;
+
+/**
+ * @BelongsProject: LeetCode
+ * @BelongsPackage: com.elvis.leetcode.contest
+ * @Author: Elvis
+ * @CreateTime: 2019-03-17 10:41
+ * Description:
+ */
+public class Leetcode1012 {
+    public int bitwiseComplement(int N) {
+        int i = 1;
+        while (N + 1 > pow(2, i)) i++;
+        int M = (int)pow(2, i) - 1;
+        return M ^ N;
+    }
+
+}
