9fa7839b854bb5bf6a8134f3db96a7052fd91072
zkstyle <712338173@qq.com>
2019-05-25 14:54:05 +0800

    leetcode

---
+++ src/com/elvis/leetcode/jdk/Test.java
@@ -0,0 +1,16 @@
+package com.elvis.leetcode.jdk;
+
+/**
+ * @BelongsProject: LeetCode
+ * @BelongsPackage: com.elvis.leetcode.jdk
+ * @Author: Elvis
+ * @CreateTime: 2019-05-24 14:38
+ * Description:
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Object object = new Object();
+        String s = "kkkk";
+    }
+}
