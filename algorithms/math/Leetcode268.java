449bad4667e0afb500c61243f7bcf4dbf7fc92d4
zkstyle <712338173@qq.com>
2019-08-16 11:38:54 +0800

    leetcode

---
+++ algorithms/math/Leetcode268.java
@@ -0,0 +1,31 @@
+package math;
+
+/**
+ * @BelongsProject: LeetCode
+ * @BelongsPackage: math
+ * @Author: Elvis
+ * @CreateTime: 2019-08-16 10:38
+ * Description:
+ * 给定一个包含 0, 1, 2, ..., n 中 n 个数的序列，找出 0 .. n 中没有出现在序列中的那个数。
+ *
+ * 示例 1:
+ *
+ * 输入: [3,0,1]
+ * 输出: 2
+ * 示例 2:
+ *
+ * 输入: [9,6,4,2,3,5,7,0,1]
+ * 输出: 8
+ * 说明:
+ * 你的算法应具有线性时间复杂度。你能否仅使用额外常数空间来实现?
+ */
+public class Leetcode268 {
+
+    public int missingNumber(int[] nums) {
+        int res = nums.length;
+        for (int i = 0; i < nums.length; i++){
+            res ^= i ^ nums[i];
+        }
+        return res;
+    }
+}
---
+++ algorithms/math/Leetcode728.java
@@ -0,0 +1,51 @@
+package math;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @BelongsProject: LeetCode
+ * @BelongsPackage: math
+ * @Author: Elvis
+ * @CreateTime: 2019-08-02 08:25
+ * Description:
+ * 自除数 是指可以被它包含的每一位数除尽的数。
+ *
+ * 例如，128 是一个自除数，因为 128 % 1 == 0，128 % 2 == 0，128 % 8 == 0。
+ *
+ * 还有，自除数不允许包含 0 。
+ *
+ * 给定上边界和下边界数字，输出一个列表，列表的元素是边界（含边界）内所有的自除数。
+ *
+ * 示例 1：
+ *
+ * 输入：
+ * 上边界left = 1, 下边界right = 22
+ * 输出： [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+ * 注意：
+ *
+ * 每个输入参数的边界满足 1 <= left <= right <= 10000。
+ */
+public class Leetcode728 {
+
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = left; i <= right; i++){
+            if (isDN(i)) list.add(i);
+        }
+        return list;
+    }
+
+    private boolean isDN(int n){
+        int tmp = n;
+        while (tmp != 0){
+            int j = tmp % 10;
+            if (j != 0 && n % j == 0){
+                tmp /= 10;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
