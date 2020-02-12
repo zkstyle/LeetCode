1f34693258cd16f6c6c5fb1604d66a071387d079
elvis <712338173@qq.com>
2020-02-12 10:46:41 +0800

    leetcode

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ If you like this project, please leave me a star. ★
 
 | No | Title | Solution | Difficulty |
 |---| ----- | -------- | ---------- |
-|　　129|　　　[Sum Root to Leaf Numbers](https://leetcode.com/problems/sum-root-to-leaf-numbers/) | 　　[java](algorithms/dfs/Leetcode129.java)|　　Medium|
+|　　15|　　　[3Sum](https://leetcode.com/problems/3sum/) | 　　[java](algorithms/array/Leetcode15.java)|　　Medium|
 |　　129|　　　[Sum Root to Leaf Numbers](https://leetcode.com/problems/sum-root-to-leaf-numbers/) | 　　[java](algorithms/dfs/Leetcode129.java)|　　Medium|
--- algorithms/array/Leetcode15.java
+++ algorithms/array/Leetcode15.java
@@ -21,110 +21,39 @@ import java.util.*;
  * ]
  */
 public class Leetcode15 {
-
-    /**
-     * 100% beats
-     * @param nums
-     * @return
-     */
-    public List<List<Integer>> threeSum_PLUS(int[] nums) {
-        if (nums.length < 3) return Collections.emptyList();
-        List<List<Integer>> res = new ArrayList<>();
-        int minValue = Integer.MAX_VALUE;
-        int maxValue = Integer.MIN_VALUE;
-        int negSize = 0; int posSize = 0;
-        int zeroSize = 0;
-        for (int v : nums) {
-            if (v < minValue) minValue = v;
-            if (v > maxValue) maxValue = v;
-            if (v > 0) posSize++;
-            else if (v < 0) negSize++;
-            else zeroSize++;
-        }
-        if (zeroSize >= 3)
-            res.add(Arrays.asList(0, 0, 0));
-        if (negSize == 0 || posSize == 0) return res;
-        if (minValue * 2 + maxValue > 0) maxValue = -minValue * 2;
-        else if (maxValue * 2 + minValue < 0) minValue = -maxValue * 2;
-        int[] map = new int[maxValue - minValue + 1];
-        int[] negs = new int[negSize];
-        int[] poses = new int[posSize];
-        negSize = 0;
-        posSize = 0;
-        for (int v : nums) {
-            if (v >= minValue && v <= maxValue) {
-                if (map[v - minValue]++ == 0) {
-                    if (v > 0)
-                        poses[posSize++] = v;
-                    else if (v < 0)
-                        negs[negSize++] = v;
-                }
-            }
-        }
-        Arrays.sort(poses, 0, posSize);
-        Arrays.sort(negs, 0, negSize);
-        int basej = 0;
-        for (int i = negSize - 1; i >= 0; i--) {
-            int nv = negs[i];
-            int minp = (-nv) >>> 1;
-            while (basej < posSize && poses[basej] < minp)
-                basej++;
-            for (int j = basej; j < posSize; j++) {
-                int pv = poses[j];
-                int cv = 0 - nv - pv;
-                if (cv >= nv && cv <= pv) {
-                    if (cv == nv) {
-                        if (map[nv - minValue] > 1)
-                            res.add(Arrays.asList(nv, nv, pv));
-                    } else if (cv == pv) {
-                        if (map[pv - minValue] > 1)
-                            res.add(Arrays.asList(nv, pv, pv));
-                    } else {
-                        if (map[cv - minValue] > 0)
-                            res.add(Arrays.asList(nv, cv, pv));
-                    }
-                } else if (cv < nv)
-                    break;
-            }
-        }
-        return res;
-    }
-
     /**
      * 常规
      * @param nums
      * @return
      */
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        // 考虑特殊情况　
+        if (nums == null || nums.length < 3) return ans;
         Arrays.sort(nums);
-        List<List<Integer>> ls = new ArrayList<>();
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {  // 跳过可能重复的答案
-
-                int l = i + 1, r = nums.length - 1, sum = 0 - nums[i];
-                while (l < r) {
-                    if (nums[l] + nums[r] == sum) {
-                        ls.add(Arrays.asList(nums[i], nums[l], nums[r]));
-                        while (l < r && nums[l] == nums[l + 1]) l++;
-                        while (l < r && nums[r] == nums[r - 1]) r--;
-                        l++;
-                        r--;
-                    } else if (nums[l] + nums[r] < sum) {
-                        while (l < r && nums[l] == nums[l + 1]) l++;   // 跳过重复值
-                        l++;
-                    } else {
-                        while (l < r && nums[r] == nums[r - 1]) r--;
-                        r--;
-                    }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) break;     //最左边的数大于0　则不可能三数之和为０
+            if (i > 0 && nums[i] == nums[i - 1])  continue;  //中心元素去重 以i为下标元素为中心元素　去L～R中寻找两个合适元素
+            int L = i + 1, R = nums.length - 1;
+            while (L < R){
+                int sum = nums[i] + nums[L] + nums[R];
+                if (sum == 0){
+                    ans.add(Arrays.asList(nums[i], nums[L], nums[R]));
+                    /**
+                     * 功能描述: 去除重复元素
+                     * 以i下标元素为中心　在L～ R中搜索合适元素 若L==L+1或R==R-1，则直接pass
+                     */
+                    while (L < R && nums[L] == nums[L + 1]) L++;
+                    while (L < R && nums[R] == nums[R - 1]) R--;
+                    L++;
+                    R--;
+                }else if (sum < 0){
+                    L++;
+                }else {
+                    R--;
                 }
             }
         }
-        return ls;
-    }
-
-    public static void main(String[] args) {
-        int[] nums = {-1, 0, 1, 2, -1, -4};
-        new Leetcode15().threeSum(nums);
+        return ans;
     }
 }
